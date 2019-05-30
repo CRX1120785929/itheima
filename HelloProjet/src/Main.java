9bf50b008d1e0252bfbc4f449d03975e1699ae4b
CRX1120785929 <1120785929@qq.com>
2019-05-31 00:46:38 +0800

    张三b的提交

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -6,10 +6,9 @@ public class Main {
     public void testHelloWrold() {
         System.out.println("HelloWorld!");
 		System.out.println("say goodBay");
-<<<<<<< HEAD
 		System.out.println("张三a的提交");
-=======
-		System.out.println("李四的提交")
->>>>>>> b85e7c9aa87c472bca555142f4643401df3fc7d2
+		System.out.println("李四的提交");
+		System.out.println("张三b的提交");
+
     }
 }
